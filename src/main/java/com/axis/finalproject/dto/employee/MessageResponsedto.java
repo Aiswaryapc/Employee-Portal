dd096d60367426e1d170ae4f1c6845357bd0b1e3
Aiswarya <aiswarya@codezend.com>
2023-01-11 08:55:46 +0530

    jwt auth

---
+++ src/main/java/com/axis/finalproject/dto/employee/MessageResponsedto.java
@@ -0,0 +1,19 @@
+package com.axis.finalproject.dto.employee;
+
+public class MessageResponsedto {
+private String message;
+
+public MessageResponsedto(String message) {
+	super();
+	this.message = message;
+}
+
+public String getMessage() {
+	return message;
+}
+
+public void setMessage(String message) {
+	this.message = message;
+}
+
+}
