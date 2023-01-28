a22a8e44bf1f5cbe40818e4bf275a97d3be14799
Sreenandha K P <kpabhishek1997@gmail.com>
2023-01-28 16:57:06 +0530

    policies

--- src/main/java/com/axis/finalproject/repository/DocumentRepository.java
+++ src/main/java/com/axis/finalproject/repository/DocumentRepository.java
@@ -3,8 +3,11 @@ package com.axis.finalproject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.axis.finalproject.entity.SalarySlip;
+import com.axis.finalproject.entity.Document;
+
+
+
 @Repository
-public interface DocumentsRepository extends JpaRepository<SalarySlip, String> {
+public interface DocumentRepository extends JpaRepository<Document, String> {
 
 }
